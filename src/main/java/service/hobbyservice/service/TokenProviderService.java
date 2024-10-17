25b8a65541eaa931dfc513e956a855a963202b91
mmingoo <aldrn0323@naver.com>
2024-10-17 21:38:22 +0900

    ✨   토큰에서 userId 추출하는 기능 추가

---
+++ src/main/java/service/hobbyservice/service/TokenProviderService.java
@@ -0,0 +1,36 @@
+package service.hobbyservice.service;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+
+@Service
+public class TokenProviderService {
+
+    private final String secretKey;
+
+    public TokenProviderService(@Value("${jwt.secret}") String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    public Long getUserIdFromToken(String token) {
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+
+        return claims.get("userId", Long.class);
+    }
+}
