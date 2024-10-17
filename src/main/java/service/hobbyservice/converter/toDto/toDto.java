74b1827b894fc17efca4e16b25c5a17a7799c6cc
mmingoo <aldrn0323@naver.com>
2024-10-17 20:12:16 +0900

    🔧  ecr 테스트

--- src/main/java/service/hobbyservice/converter/toDto/toDto.java
+++ src/main/java/service/hobbyservice/converter/toDto/toDto.java
@@ -7,7 +7,7 @@ import service.hobbyservice.entity.HobbyRoutine;
 public class toDto {
 
 
-    //record 객체를 Dto로 변환화는 메서드
+    //record 객체를 Dto로 변환화는 메서드이다
     public static HobbyResponseDto.HobbyRecordDto toHobbyRecordDto(HobbyRecord hobbyRecord, HobbyRoutine hobbyRoutine){
         return HobbyResponseDto.HobbyRecordDto.builder()
                 .hobbyBody(hobbyRecord.getRecordBody())
